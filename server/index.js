f8f5b45b99a5663a54557247b477c4dc2a5aba8d
saidnahum <70162272+saidnahum@users.noreply.github.com>
2022-03-28 16:26:43 -0600

    servidor creado, configurado y dividido

--- server/index.js
+++ server/index.js
@@ -1,13 +1,7 @@
-import express from 'express';
-import postsRoutes from './routes/posts.routes.js';
 import { connectDB } from './db.js';
 import { PORT } from './config.js';
-
-const app = express();
+import app from './app.js';
 
 connectDB();
-
-app.use(postsRoutes);
-
 app.listen(PORT);
 console.log('Server is running on port', PORT);
